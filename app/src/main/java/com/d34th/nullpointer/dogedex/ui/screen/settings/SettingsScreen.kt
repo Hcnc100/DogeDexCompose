c9bf203246c49e4df0ddbdde2a4c32a00701ec0d
Death <ricardo.pajaro.bc2d@gmail.com>
2022-09-04 22:09:10 -0500

    show info user in settings screen

--- app/src/main/java/com/d34th/nullpointer/dogedex/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/d34th/nullpointer/dogedex/ui/screen/settings/SettingsScreen.kt
@@ -1,16 +1,17 @@
 package com.d34th.nullpointer.dogedex.ui.screen.settings
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.ExtendedFloatingActionButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.d34th.nullpointer.dogedex.R
+import com.d34th.nullpointer.dogedex.core.states.AuthState
+import com.d34th.nullpointer.dogedex.models.User
 import com.d34th.nullpointer.dogedex.presentation.AuthViewModel
 import com.d34th.nullpointer.dogedex.ui.share.ToolbarBack
 import com.ramcosta.composedestinations.annotation.Destination
@@ -22,6 +23,9 @@ fun SettingsScreen(
     authViewModel: AuthViewModel,
     navigator: DestinationsNavigator
 ) {
+
+    val stateUser by authViewModel.stateUser.collectAsState()
+
     Scaffold(
         topBar = {
             ToolbarBack(
@@ -35,8 +39,42 @@ fun SettingsScreen(
                 text = { Text(text = stringResource(R.string.text_button_sign_out)) },
                 onClick = authViewModel::signOut
             )
-        }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) {
-        Box(modifier = Modifier.padding(it))
+        when (val currentUser = stateUser) {
+            is AuthState.Authenticated -> {
+                UserAuthInfo(
+                    modifier = Modifier.padding(it),
+                    user = currentUser.currentUser
+                )
+            }
+            else -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserAuthInfo(
+    modifier: Modifier = Modifier,
+    user: User
+) {
+    Column(modifier = modifier.padding(10.dp)) {
+        Text(text = stringResource(R.string.title_info_user), style = MaterialTheme.typography.h5)
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(text = stringResource(R.string.title_id_user, user.id))
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = stringResource(R.string.title_email_user, user.email))
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = stringResource(R.string.title_token_user, user.token))
     }
 }
