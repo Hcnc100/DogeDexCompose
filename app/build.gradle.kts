eaa82e28fc1f90985b38db8a1754cf54357ac8b0
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-01-27 19:54:47 -0600

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.d34th.nullpointer.dogedex"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 34
         versionCode = 2
         versionName = "2.0"
 
@@ -71,36 +71,36 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.compose.ui:ui:1.6.0")
     implementation("androidx.compose.material:material:1.6.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
-    implementation("androidx.test.ext:junit-ktx:1.1.3")
-    implementation("androidx.test:runner:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("androidx.test:runner:1.5.2")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0")
 
     // * coil
-    implementation("io.coil-kt:coil-compose:2.1.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
     // *lottie compose
-    implementation("com.airbnb.android:lottie-compose:5.1.1")
+    implementation("com.airbnb.android:lottie-compose:6.3.0")
 
     // * timber
     implementation("com.orhanobut:logger:2.2.0")
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
     // * hilt
     val daggerHiltVersion = "2.50"
     implementation("com.google.dagger:hilt-android:${daggerHiltVersion}")
     kapt("com.google.dagger:hilt-compiler:${daggerHiltVersion}")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     // ? hilt test
     testImplementation("com.google.dagger:hilt-android-testing:${daggerHiltVersion}")
     androidTestImplementation("com.google.dagger:hilt-android-testing:${daggerHiltVersion}")
@@ -109,21 +109,21 @@ dependencies {
     // * room
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:${roomVersion}")
-    kapt("androidx.room:room-compiler:${roomVersion}")
+    ksp("androidx.room:room-compiler:${roomVersion}")
     implementation("androidx.room:room-ktx:${roomVersion}")
     testImplementation("androidx.room:room-testing:${roomVersion}")
 
     // * save state
-    implementation("androidx.savedstate:savedstate-ktx:1.2.0")
+    implementation("androidx.savedstate:savedstate-ktx:1.2.1")
 
     // * image compressor
     implementation("com.github.Shouheng88:compressor:1.6.0")
 
     // * splash screen
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     // * shimmer effect
-    implementation("com.valentinilk.shimmer:compose-shimmer:1.0.3")
+    implementation("com.valentinilk.shimmer:compose-shimmer:1.2.0")
 
     // * navigation
     val destinationsVersion = "1.10.0"
@@ -133,8 +133,6 @@ dependencies {
     // * data store
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    // * gson
-    implementation("com.google.code.gson:gson:2.8.9")
 
     // * Retrofit
     val retrofitVersion = "2.9.0"
@@ -146,12 +144,12 @@ dependencies {
     testImplementation("com.squareup.okhttp3:mockwebserver:4.8.1")
 
     // * camera
-    implementation("androidx.camera:camera-camera2:1.1.0")
-    implementation("androidx.camera:camera-lifecycle:1.1.0")
-    implementation("androidx.camera:camera-view:1.1.0")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
 
     // * permission
-    implementation("com.google.accompanist:accompanist-permissions:0.26.1-alpha")
+    implementation("com.google.accompanist:accompanist-permissions:0.28.0")
 
     // * Tensorflow
     implementation("org.tensorflow:tensorflow-lite:2.9.0")
@@ -159,15 +157,15 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-support:0.4.2")
 
     // ? android test
-    testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    androidTestImplementation("androidx.test:rules:1.4.0")
-    androidTestUtil("androidx.test:orchestrator:1.4.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestUtil("androidx.test:orchestrator:1.4.2")
 
     implementation("com.google.accompanist:accompanist-swiperefresh:0.25.1")
 
     // * kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
 }
